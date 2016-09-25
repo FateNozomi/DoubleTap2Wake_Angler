3e87c446ea05df613f8575f2e50b269bdb8cf139
William Liew <William Liew>
2016-09-25 18:28:55 +0800

    Implement SharedPreferences for DT2WState

--- app/src/main/java/com/fate/android/doubletap2wakeangler/MainActivity.java
+++ app/src/main/java/com/fate/android/doubletap2wakeangler/MainActivity.java
@@ -1,13 +1,21 @@
 package com.fate.android.doubletap2wakeangler;
 
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import static android.R.id.edit;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Initialize constants for isDT2WEnabled() SharedPreferences
+    private static final String SAVED_STATE = "DT2W_STATE";
+    private static final String PREFERENCES_KEY = "com.fate.android.doubletap2wakeangler";
 
     private TextView deviceAngler;
     private Button enableDT2W;
@@ -31,12 +39,14 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     DT2W(true);
+                    DT2WState(true);
                 }
             });
             disableDT2W.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     DT2W(false);
+                    DT2WState(false);
                 }
             });
         } else {
@@ -64,5 +74,29 @@ public class MainActivity extends AppCompatActivity {
         String command = "echo " + (enable ? "1" : "0") +
                 " > /sys/devices/soc.0/f9924000.i2c/i2c-2/2-0070/input/input0/wake_gesture";
         RootUtil.runAsRoot(command);
+    }
+
+    private boolean DT2WState(boolean isEnabled){
+        /**
+         * Access the shared preferences file that's identified by the resource
+         * string R.string.PREFERENCES_KEY and open it using the private mode so
+         * the file is accessible by only this app.
+         */
+        SharedPreferences prefs =
+                getApplicationContext()
+                .getSharedPreferences(PREFERENCES_KEY, MODE_PRIVATE);
+
+        /**
+         * Assign a boolean value into SAVED_STATE
+         */
+        prefs.edit().putBoolean(SAVED_STATE, isEnabled).apply();
+
+        /**
+         * Retrieve a boolean value from the SAVED_STATE
+         * If SERVICE_STATUS_FLAG does not exist, set to false.
+         */
+        boolean savedState = prefs.getBoolean(SAVED_STATE, false);
+        Log.d("MainActivity", "prefs: " + savedState);
+        return savedState;
     }
 }
